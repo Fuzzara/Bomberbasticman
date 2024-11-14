ed2407e479de02983d163c6fc8daaf4f687ab6e2
Felipe González <l.gonzalez.4@estudiantec.cr>
2024-11-13 21:58:25 -0600

    Add punto coords, personaje, background, colliders, final final final final

--- core/src/main/java/io/github/JFW/Main.java
+++ core/src/main/java/io/github/JFW/Main.java
@@ -2,61 +2,92 @@ package io.github.JFW;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.awt.*;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private Stage stage;
     private Skin skin;
+    private SpriteBatch batch;
+    private Texture background;
+    private Texture uiBackground;
+    private CollisionSystem collSystem;
+    //TESTEO
+    private Player player;
+    public Array<Rectangle> obstacles;
+    private ShapeRenderer sr;
+
+    //CUADRITO DEBUG
+    private Rectangle rect = new Rectangle(90,45,2,2);
 
     @Override
     public void create() {
-        stage = new Stage(new FitViewport(640, 480));
+        stage = new Stage(new ExtendViewport(768, 672)); //
         skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        batch = new SpriteBatch();
 
-        Window window = new Window("Example screen", skin, "border");
-        window.defaults().pad(4f);
-        window.add("This is a simple Scene2D view.").row();
-        final TextButton button = new TextButton("Click me!", skin);
-        button.pad(8f);
-        button.addListener(new ChangeListener() {
-            @Override
-            public void changed(final ChangeEvent event, final Actor actor) {
-                button.setText("Clicked.");
-            }
-        });
-        window.add(button);
-        window.pack();
-        // We round the window position to avoid awkward half-pixel artifacts.
-        // Casting using (int) would also work.
-        window.setPosition(MathUtils.roundPositive(stage.getWidth() / 2f - window.getWidth() / 2f),
-            MathUtils.roundPositive(stage.getHeight() / 2f - window.getHeight() / 2f));
-        window.addAction(Actions.sequence(Actions.alpha(0f), Actions.fadeIn(1f)));
-        stage.addActor(window);
+        background = new Texture(Gdx.files.internal("bg.png"));
+        uiBackground = new Texture(Gdx.files.internal("uibg.png"));
 
         Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render() {
+        draw();
+        input();
+        player.update();
+    }
+    private void draw() {
         ScreenUtils.clear(0f, 0f, 0f, 1f);
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+        batch.begin();
+        batch.draw(background, 0, 0, 768, 576);
+        batch.draw(uiBackground, 0, 576, 768, 96);
+        batch.end();
+
+        //DEBUG
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        sr.setColor(0,1,0,1);
+        sr.rect(rect.x, rect.y, rect.width, rect.height);
+        sr.end();
+
+    }
+
+    private void input(){
+        if (Gdx.input.isKeyPressed(Input.Keys.J)) {
+            rect.x -= .5;
+            Gdx.app.log("COORDS CUADRITO", "X: " + rect.x + " Y: " + rect.y);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.K)) {
+            rect.y -= .5;
+            Gdx.app.log("COORDS CUADRITO", "X: " + rect.x + " Y: " + rect.y);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.L)) {
+            rect.x += .5;
+            Gdx.app.log("COORDS CUADRITO", "X: " + rect.x + " Y: " + rect.y);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.I)) {
+            rect.y += .5;
+            Gdx.app.log("COORDS CUADRITO", "X: " + rect.x + " Y: " + rect.y);
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height);
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
