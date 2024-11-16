bf7b8a148b6c8a3e79fe9e199d521c6c72662e24
Felipe González <l.gonzalez.4@estudiantec.cr>
2024-11-16 15:34:13 -0600

    texturas enemigos, no tanta cosa

--- core/src/main/java/io/github/JFW/Enemies.java
+++ core/src/main/java/io/github/JFW/Enemies.java
@@ -1,6 +1,7 @@
 package io.github.JFW;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -19,6 +20,8 @@ public class Enemies extends Actor{
     private Sprite enemySprite;
     private Texture enemyTexture;
 
+    private SpriteBatch batch;
+    private Map currentMap;
 
     private Rectangle boundingBox;
 
@@ -27,7 +30,29 @@ public class Enemies extends Actor{
         this.position = new Vector2(x,y);
         this.score = score;
         this.ai = ai;
+
+        //por mientras, cambiar!
+        this.enemyTexture = new Texture("bombwip.png");
+        this.enemySprite = new Sprite(enemyTexture);
+        this.enemySprite.setSize(SPRITE_WIDTH,SPRITE_HEIGHT);
+        this.enemySprite.setPosition(position.x,position.y);
+        this.boundingBox = new Rectangle(position.x,position.y,BOUNDING_BOX_SIZE,BOUNDING_BOX_SIZE);
+
     }
 
+    public void draw(){
+        batch.begin();
+        enemySprite.draw(batch);
+        batch.end();
+    }
+
+    public void update(){
+        draw();
+    }
+
+    public void setCurrentMap(Map map) {
+        this.currentMap = map;
+    }
 
 }
+
